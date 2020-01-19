d4d9142f1fa6b2e3f1addfe9b2d02168ce4914e8
Maciej Sznurawa <msznurawa@gmail.com>
2020-01-19 09:12:36 +0100

    Add search countries functionality

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries } from '../actions/actions-countries';
+import { getCountries, searchCountries } from '../actions/actions-countries';
 
 class CountryFlagContainer extends React.Component {
 
   // odwołanie do store metoda dispatch
   componentDidMount() {
-    this.props.dispatch(getCountries());
+    this.props.dispatch(searchCountries(''));
+  }
+
+  search(e) {
+    this.props.dispatch(searchCountries(e.target.value))
   }
 
   render() {
     return (
       <div>
-        <CountryFlagList countries={this.props.countries} />
+        <div className="search text-center">
+          <input type="text" onChange={this.search.bind(this)} />
+        </div>
+        <CountryFlagList countries={this.props.visibleCountries} />
       </div>
     )
   }
@@ -22,7 +29,8 @@ class CountryFlagContainer extends React.Component {
 // stan aplikacji jest mapowany do propsa countries w komponencie CountryFlagContainer skąd jest przekazywany jako parametr do propsa countries komponentu CountryFlagList który w ten sposób otrzymuje tablice obiektów countries
 const mapStateToProps = function (store) {
   return {
-    countries: store.countriesReducer.countries
+    countries: store.countriesReducer.countries,
+    visibleCountries: store.countriesReducer.visibleCountries
   };
 };
 
